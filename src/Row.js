85bb2d1390efb987976ae4ddfdb827d5b88cb1c2
Jonathanbir <jonathan79111@gmail.com>
2022-03-21 22:30:31 +0800

    commit  netfilx web

--- src/Row.js
+++ src/Row.js
@@ -5,7 +5,7 @@ import YouTube from "react-youtube";
 import axios from "./axios";
 import "./Row.css";
 
-const base_url = "https:/image.tmdb.org/t/p/original/";
+const base_url = "https://image.tmdb.org/t/p/original";
 
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
